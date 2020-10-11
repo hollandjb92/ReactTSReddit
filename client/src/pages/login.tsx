eb92d71a62527ac68e6633e1d9b74037536090d8
Jordan Holland <hollandjb@vcu.edu>
2020-10-10 23:37:17 -0400

    added login page and form

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -3,20 +3,20 @@ import {Formik, Form} from 'formik'
 import { Box, Button} from '@chakra-ui/core';
 import  {Wrapper} from '../components/Wrapper';
 import { InputField } from '../components/InputField';
-import { useRegisterMutation } from '../generated/graphql';
+import { useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from 'next/router';
 
-const Register: React.FC<{}> = ({}) => {
+const Login: React.FC<{}> = ({}) => {
 		const router = useRouter();
-		const [{},register] = useRegisterMutation();
+		const [{},login] = useLoginMutation();
 		return (
 			<Wrapper variant="small">
 				<Formik initialValues={{username: "", password: ""}} onSubmit={async (values, {setErrors}) => {
-					const response = await register(values)
-					if(response.data?.register.errors) {
-						setErrors(toErrorMap(response.data.register.errors))
-					} else if (response.data?.register.user){
+					const response = await login({options: values})
+					if(response.data?.login.errors) {
+						setErrors(toErrorMap(response.data.login.errors))
+					} else if (response.data?.login.user){
 						//got user back
 						router.push("/")
 					}
@@ -27,7 +27,7 @@ const Register: React.FC<{}> = ({}) => {
 						<InputField name="username" placeholder="username" label="Username" />
 						<InputField name="password" placeholder="password" label="Password" type="password"/>
 						</Box>
-						<Button mt={4} type="submit" isLoading={isSubmitting} variantColor="teal">register</Button>
+						<Button mt={4} type="submit" isLoading={isSubmitting} variantColor="teal">login</Button>
 					</Form>
 				)}
 				</Formik>
@@ -35,4 +35,4 @@ const Register: React.FC<{}> = ({}) => {
 		);
 }
 
-export default Register;
+export default Login;
